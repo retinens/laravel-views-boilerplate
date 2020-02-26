2f4eb7aa72ad4d189fab818a76a1f87df7906492
Lucas Houssa <lucas@retinens.be>
2020-02-26 13:54:29 +0100

    first_commit

---
+++ layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    {!! app('seotools')->generate() !!}
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="icon" href="/images/logos/favicon.png">
+    <link rel="stylesheet" href="{{ mix('/app/css/app.css') }}">
+</head>
+
+<body class="">
+<div class="justify-content-center d-flex w-100">
+    @include('cookieConsent::index')
+</div>
+{{-- NAV --}}
+@include('layouts.header')
+<div style="margin-top: 70px">
+    @yield('content')
+</div>
+{{-- CONTENT --}}
+
+@include('layouts.footer')
+<script src="{{mix('/app/js/manifest.js')}}"></script>
+<script src="{{mix('/app/js/vendor.js')}}"></script>
+<script src="{{mix('/app/js/app.js')}}"></script>
+@yield('scripts')
+</body>
+</html>
